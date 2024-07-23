f06e4ad5d4a5dee8b817303bcf8909fa40bdce7d
precious123-gifted <ogunameprecious123@gmail.com>
2024-07-23 20:22:37 +0100

    node mailer

--- slices/Enquiry/servermailer.ts
+++ slices/Enquiry/servermailer.ts
@@ -13,7 +13,7 @@ export default async function submitForm(formData: FormData) {
   await sendMail({
     to: "formosa6js@gmail.com",
     name: `${formData.name}`,
-    subject: `FORMOSA Oil and Gas enquiry from ${formData.name}`,
+    subject: `FORMOSA Marine Services enquiry from ${formData.name}`,
     body: `<p>Senders Company Name: ${formData.company_name}</p><br><p>Senders Email: ${formData.email}</p><br><p>Senders Phone Number: ${formData.phone_number}</p><br><h1>${formData.enquiry}</h1>`,
   });
 
